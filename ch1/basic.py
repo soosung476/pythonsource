e03edd6822912ea9ca8211c981b89fcbb7bac08f
soosung <soosung001@gmail.com>
2026-07-14 17:20:32 +0900

    basic python grammar1

---
+++ ch1/basic.py
@@ -0,0 +1,17 @@
+
+a = 123
+print(a);
+
+a = -178;
+print(a);
+
+a = 1.2
+print(a);
+
+a = -3.14
+print(a);
+multiline = """Life is too short,
+You need Python"""
+print(multiline);
+multiline = '''Life is too short,\nYou need Python'''
+print(multiline);
