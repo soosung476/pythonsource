f713adb705609fad031b52109fb0e1c6814e8583
soosung kim <soosung001@gmail.com>
2026-08-27 09:37:39 +0900

    change miner bug, add res.encoding line to remove replace grammar

--- Bigpy/chapter03/scrap12.py
+++ Bigpy/chapter03/scrap12.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup
 url = "http://books.toscrape.com/"
 
 res = requests.get(url)
+res.encoding = 'utf-8'
 soup =BeautifulSoup(res.text, 'html.parser')
 
 books = soup.select("article.product_pod")
 for book in books:
     title =book.select_one("h3 > a")['title']
-    price = book.select_one("p.price_color").text.replace("Â","")
+    price = book.select_one("p.price_color").text
     rating = book.select_one("p.star-rating")["class"][1]
 
     print(f"{title} | {price} | 별점 {rating}")
